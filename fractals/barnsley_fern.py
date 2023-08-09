92390b26e1f9b377157de1e7dee086bcc91049c1
Lars Moan <lars.ostberg.moan@gmail.com>
2023-08-09 13:43:20 +1000

    Switched to poetry for dependency control

---
+++ fractals/barnsley_fern.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+#Create a window of white pixels
+window = np.zeros((500, 500, 3), dtype=np.uint8)
+window.fill(255)
--- fractals/gaussian_blur.py
+++ fractals/gaussian_blur.py
@@ -6,7 +6,7 @@ print("PyTorch Version:", torch.__version__)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using {device} for computation")
 
-def gaussian():
+def gaussian(variance:int =4):
     #grid for computing image, subdivide the space
     X, Y = np.mgrid[-4.0:4:0.01, -4.0:4:0.01]
     x = torch.tensor(X)
@@ -15,7 +15,8 @@ def gaussian():
     x = x.to(device)
     y = y.to(device)
     #Compute the gaussian "blur?"
-    z = torch.exp(-(x**2+y**2)/2.0)
+    z = 1/(2*np.pi*variance)*torch.exp(-(x**2+y**2)/(2*variance))
+
     return z
 
 #Change the Gaussian function into a 2D sine function
@@ -23,7 +24,8 @@ def sine(a=1, b=0):
     X, Y = np.mgrid[-4.0:4:0.01, -4.0:4:0.01]
     x = torch.tensor(X)
     y = torch.tensor(Y)
-    z = torch.sin(a*torch.sqrt(x**2 + y**2)+b)
+    #z = torch.sin(a*torch.sqrt(x**2 + y**2)+b)
+    z = torch.sin(x+y)
     return z
 
 def visualize(result):
@@ -36,7 +38,7 @@ def visualize(result):
 
 if __name__ == "__main__":
     #Part 1
-    visualize(sine(a=2,b=0))
+    #visualize(sine(a=2,b=0))
     #Multiplying the two dimensional sine function with the gaussian, makes sense that this "dampens" out the waves.
     visualize(gaussian() * sine())
 
